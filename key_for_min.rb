aeb1a4bffcf43ef3d27490553464b77658b3790a
Jon Schulman <schulman99@gmail.com>
2018-06-27 17:44:40 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  # if name_hash = {}
+  # if name_hash == {}
   #   nil
   # end
 
   # else
-  small_key = ""
-  small_value = Float::INFINITY
+  small_key = nil
+  small_value = nil
   name_hash.select do |key, value|
-    if value < small_value
+    if small_value == nil || value < small_value
       small_value = value
       small_key = key
     end
